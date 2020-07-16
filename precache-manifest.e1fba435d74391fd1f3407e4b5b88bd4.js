e82624d72ae5d09d2c07e110c8615b8f687a4507
ruddyyhit <47591711+ruddyyhit@users.noreply.github.com>
2020-07-16 15:49:15 +0530

    Updates

---
+++ precache-manifest.e1fba435d74391fd1f3407e4b5b88bd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6632773eef7e59ff17b64f931ca518fa",
+    "url": "/ruddyyhit/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "d2600669a7d368959c75",
+    "url": "/ruddyyhit/monsters-rolodex.git/static/css/main.0cbbb316.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/ruddyyhit/monsters-rolodex.git/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ruddyyhit/monsters-rolodex.git/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2600669a7d368959c75",
+    "url": "/ruddyyhit/monsters-rolodex.git/static/js/main.ee7b8c59.chunk.js"
+  },
+  {
+    "revision": "718e8a93b3ae2c39d40b",
+    "url": "/ruddyyhit/monsters-rolodex.git/static/js/runtime-main.c730d41e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ruddyyhit/monsters-rolodex.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
